e8e403d8570d362cc38132db587d8aa894ac1130
Valeriy <fedosv.valera95@gmail.com>
2017-12-04 19:47:15 +0200

    jpa repository added query for search works not completely

---
+++ src/main/java/edu/karazin/shop/repository/ProductRepository.java
@@ -0,0 +1,24 @@
+package edu.karazin.shop.repository;
+
+import edu.karazin.shop.model.Product;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ProductRepository extends CrudRepository<Product, Long>{
+
+    List<Product> findAll();
+
+    Product getProductById(Long id);
+
+    @Query(value = "select p from Product p where p.description like :searchText or p.title like :searchText")
+    List<Product> findByText(@Param("searchText") String searchText);
+
+    Product save(Product prod);
+
+    void deleteById(Long id);
+
+}
+
